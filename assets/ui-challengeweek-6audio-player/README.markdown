c8ed8aaa0cf5c42cd4f59c4434b92b55349f23f7
oscarbahamonde <dev@oscarbahamonde.cloud>
2022-01-11 02:42:29 -0500

    first commit

---
+++ assets/ui-challengeweek-6audio-player/README.markdown
@@ -0,0 +1,5 @@
+# UI Challenge - Week 6 - Audio Player
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/mattstvartak/pen/WQBvMr](https://codepen.io/mattstvartak/pen/WQBvMr).
+
+
---
+++ assets/ui-challengeweek-6audio-player/dist/script.js
@@ -0,0 +1,12 @@
+(function($) {
+		$(document).ready(function() {
+
+			// Pause/Play functionality
+			var playButton = $('.control-play'),
+				album = $('.album');
+
+			playButton.on('click', function() {
+				$('.music-player-container').toggleClass('is-playing');
+			});
+		});
+	})(jQuery);
